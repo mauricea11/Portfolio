7121be904f968f7552f03078f65c87cfa99f3277
mauricea11 <mauriceaugust117@gmail.com>
2022-11-06 22:56:29 -0500

    fonts and loading screen

--- src/App.js
+++ src/App.js
@@ -1,23 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import React, { useState } from "react";
 
 function App() {
+  const [loadingNums, changeLoadingNums] = useState(0);
+
+  function increLoadingNums() {
+    changeLoadingNums(loadingNums + 1);
+    console.log(loadingNums);
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1 className="loading-text">LOADING </h1>
+      <h2 className="loading-text">{loadingNums}%</h2>
+      <button onClick={increLoadingNums}>click</button>
     </div>
   );
 }
